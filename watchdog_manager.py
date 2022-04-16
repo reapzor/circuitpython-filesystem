04dcdb7bd7f0f14b00342f82db6815f98c6b20ba
Charles Benson <charles.benson@sonos.com>
2022-04-16 10:46:59 -0400

    add watchdog manager

---
+++ watchdog_manager.py
@@ -0,0 +1,29 @@
+from async_tasks import async_tasks
+from microcontroller import watchdog
+from watchdog import WatchDogMode
+
+
+class WatchdogManager:
+
+    def __init__(self):
+        # 10 minutes
+        self.timeout = 60 * 10
+        self.feed_task = None
+
+    def start_watchdog(self):
+        watchdog.timeout = self.timeout
+        watchdog.mode = WatchDogMode.RAISE
+        watchdog.feed()
+
+    def feed(self):
+        watchdog.feed()
+
+    def auto_feed(self):
+        if not self.feed_task:
+            self.feed_task = async_tasks.every(10000, self.__auto_feed)
+
+    async def __auto_feed(self):
+        self.feed()
+
+
+watchdog_manager = WatchdogManager()
