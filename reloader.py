5ea20d0c200201786d2c0bbc89dfd145b3b398f8
Charles Benson <charles.benson@sonos.com>
2022-04-17 15:13:58 -0400

    cleanup

--- reloader.py
+++ reloader.py
@@ -1,4 +1,5 @@
 import supervisor
+import microcontroller
 from persisted_ram import persisted_ram
 from persisted_storage import persisted_storage
 
@@ -8,12 +9,19 @@ class Reloader:
     def __init__(self):
         pass
 
-    def reload(self):
+    def __save(self):
         if persisted_ram.loaded:
             persisted_ram.save()
         if persisted_storage.loaded:
             persisted_storage.save()
+
+    def reload(self):
+        self.__save()
         supervisor.reload()
+
+    def reset(self):
+        self.__save()
+        microcontroller.reset()
 
 
 reloader = Reloader()
